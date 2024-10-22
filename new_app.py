70a23afbde75ddd4ab9a5a2a2c6d10005cdd027d
Lanalama-cloud <lanulichka2709@gmail.com>
2024-10-22 09:27:47 +0300

    Add all project files

---
+++ new_app.py
@@ -0,0 +1,14 @@
+print("I'm learning Git merge conflicts")
+
+# Ask for the user's first name
+name = input('Please enter your first name: ')
+
+# Ask for the user's last name
+last_name = input('Please enter your last name: ')
+
+# Ask for the user's nickname
+nick = input('Please enter your nickname: ')
+
+# Print a greeting message using all three variables
+print('Hi, {} {}!'.format(name, last_name) + 'a.k.a. {}!'.format(nick))
+
